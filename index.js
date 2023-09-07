0f1c749209c31b5d5a68f308fee30d8d05738479
thereson <gerardokolie09@gmail.com>
2023-09-07 06:25:36 -0700

    updatd time format

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ server.get("/api", (req, res) => {
   let responce = {
     slack_name: req.query.slack_name,
     current_day: weekdays[new Date().getDay() - 1],
-    utc_time: new Date(),
+    utc_time: new Date().toISOString(),
     track: req.query.track,
     github_file_url:
       "https://github.com/thereson/student_access_endpint/index.js",
