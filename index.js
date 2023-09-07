5ecdd4170dfa67e22399d62fc8108a62c63699eb
thereson <gerardokolie09@gmail.com>
2023-09-07 04:18:36 -0700

    create a basic server for accepting get request

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+let server = express();
+let weekdays = [
+  "monday",
+  "tuesday,",
+  "wednesday",
+  "thurday",
+  "friday",
+  "saturday",
+  "sunday",
+];
+
+server.get("/:slack_name/:track", (req, res) => {
+  let responce = {
+    slack_name: req.params.slack_name,
+    current_day: weekdays[new Date().getDay() - 1],
+    utc_time: new Date(),
+    track: req.params.track,
+    github_file_url:
+      "https://github.com/thereson/student_access_endpint/index.js",
+    github_repo_url: "https://github.com/thereson/student_access_endpint",
+    status_code: 200,
+  };
+  res.send(JSON.stringify(responce));
+});
+
+server.listen(9000, () => {
+  console.log("server running on port 9000");
+});
