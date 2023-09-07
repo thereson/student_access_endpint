f6cc0f612a7b99ae75c694cf7a225952ab591ced
thereson <gerardokolie09@gmail.com>
2023-09-07 12:50:26 -0700

    updated time format

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ const express = require("express");
 
 let server = express();
 let weekdays = [
-  "Monday",
-  "Tuesday,",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday",
+  "monday",
+  "tuesday,",
+  "wednesday",
+  "thurday",
+  "friday",
+  "saturday",
+  "sunday",
 ];
+let [first, last] = new Date().toISOString().split(".");
 
 server.get("/api", (req, res) => {
   let responce = {
     slack_name: req.query.slack_name,
     current_day: weekdays[new Date().getDay() - 1],
-    utc_time: new Date().toISOString(),
+    utc_time: first + "Z",
     track: req.query.track,
     github_file_url:
       "https://github.com/thereson/student_access_endpint/index.js",
