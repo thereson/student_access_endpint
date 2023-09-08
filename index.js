78708025b7f4df88a8244a2c775418f56a1992ab
thereson <gerardokolie09@gmail.com>
2023-09-07 22:32:59 -0700

    updating time utc

--- index.js
+++ index.js
@@ -11,15 +11,36 @@ let weekdays = [
   "Sunday",
 ];
 let [first, last] = new Date().toISOString().split(".");
+const now = new Date();
 
-first = first.replace(/(\r\n|\n|\r)/g, "-");
+const year = now.getUTCFullYear();
+const month = now.getUTCMonth() + 1;
+const day = now.getUTCDate();
+const hours = now.getUTCHours();
+const minutes = now.getUTCMinutes();
+const seconds = now.getUTCSeconds();
+const milliseconds = now.getUTCMilliseconds();
+
+let date =
+  year +
+  "-0" +
+  month +
+  "-0" +
+  day +
+  "T0" +
+  hours +
+  ":" +
+  minutes +
+  ":" +
+  seconds +
+  "Z";
 
 server.get("/api", (req, res) => {
   let responce = {
     slack_name: req.query.slack_name,
     current_day: weekdays[new Date().getDay() - 1],
+    utc_time: date,
     track: req.query.track,
-    utc_time: first + "Z",
     github_file_url:
       "https://github.com/thereson/student_access_endpint/index.js",
     github_repo_url: "https://github.com/thereson/student_access_endpint",
